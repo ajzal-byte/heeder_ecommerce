e18ecfb1539cc1da131e9d50fc2ec5fce76da052
ajzal-byte <ajzalaju10@gmail.com>
2023-11-30 12:04:27 +0530

    add to cart

--- controller/user_controller/user_cart.js
+++ controller/user_controller/user_cart.js
@@ -22,31 +22,38 @@ module.exports.getCart = async (req, res)=>{
 module.exports.addtoCart = async (req, res)=>{
   try{
     const userSession = req.session.user
-    console.log(userSession);
     if(userSession){
-      // console.log(userSession);
-      // console.log(req.session.user);
-      const productID = req.query.productID;
-      const product = await productCollection.findById(productID);
+      const productId = req.query.productId;
+      const product = await productCollection.findById(productId);
       const user = await userCollection.findOne({email: userSession.email});
-      const userCart = await cartCollection.findOne({userID: user._id}) 
+      const userCart = await cartCollection.findOne({userId: user._id});
       // if user has a cart
       if(userCart){
-        let productIndex = userCart.products.findIndex(p => p.productID == productID)
+        let productIndex = userCart.products.findIndex(p => p.productId == productId)
         //if product exists
         if(productIndex > -1){
+          console.log('quantity updation');
           let productItem = userCart.products[productIndex];
           productItem.quantity += 1;
           userCart.products[productIndex] = productItem;
         }else{
-          userCart.products.push({productID, quantity})
+          console.log('product addition');
+          userCart.products.push({productId: product._id, quantity:1})
         }
+          // Save the changes to the database
+          await userCart.save();
+      }else{
+        console.log(user._id);
+        console.log('cart creation');
+          await cartCollection.create({
+          userId: user._id, 
+          products:[{productId: product._id, quantity:1}]})
       }
+      res.status(200).json({message: "Item Added to Cart"})
 
     }else{
-      console.log(userSession);
-      console.log('Redirecting to /login'); 
-      res.redirect('/login')
+      console.log('not logged in')
+      res.status(200).json({ error: "User not logged in" });
     }
     
   }catch(error){
